0111630284b3165b7dbc9068bd16616b9ac2c6c9
Halldór Valberg <halldorvs17@ru.is>
2019-11-26 17:34:55 +0000

    Boards, lists and tasks

--- Toddler/src/toddler-expo/components/board/index.js
+++ Toddler/src/toddler-expo/components/board/index.js
@@ -1,39 +1,79 @@
 import React from 'react';
 import { View, Text, Image, TouchableHighlight, StyleSheet} from 'react-native';
+import DATA from '../../data/data.json'
 
 
 //Simply print board name
 class Viewable_Boards extends React.Component {
-    _func = () => {
-        console.log('It happened')
+    state = {
+        all_boards: DATA.boards
     }
-
     render() {
-        const { id, src, name } = this.props;
-        return(
-            <View style={styles.container}>
-                <Text>{name}</Text>
-
-                <TouchableHighlight onPress={this._func}>
-                    <Image style={{width: 100, height: 100}} source={{uri: src}} />   
-                </TouchableHighlight>
-
-                
+        return (
+            <View>
+                {this.state.all_boards.map(({id, name, thumbnailPhoto}) => <View_Board key={id} name={name} src={thumbnailPhoto}/>)}
             </View>
         )
     }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1, 
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        backgroundColor: 'lightskyblue',
-        alignItems: 'center',
-        justifyContent: 'center'
-
+class View_Board extends React.Component {
+    state = {
+        board_lists: DATA.lists
     }
-});
+
+    render() {
+        const list = this.state.board_lists.filter(list => list.BoardId === this.props.id)
+        const {name, src} = this.props;
+        return(
+            <View>
+                <Image source={{uri: src}} style={{height: 100, width: 100}} />
+                <Text>
+                    Board: {name}
+                </Text>
+                {list.map(({id, name, color}) => <View_List key={id} name={name} color={color} id={id}/>)}
+            </View>
+        )
+    }
+}
+
+class View_List extends React.Component {
+    state = {
+        list_tasks: DATA.tasks
+    }
+
+    render() {
+        const tasks = this.state.list_tasks.filter(task => task.listId === this.props.id)
+        const{name, color} = this.props;
+        return(
+            <View>
+                <Text>
+                    List: {name} (should be in color {color})
+                </Text>
+                {tasks.map(({name, description, isFinished}) => <View_Task key={id} name={name} description={description} isFinished={isFinished} />)}
+            </View>
+        )
+    }
+}
+
+class View_Task extends React.Component {
+    render() {
+        const {name, description, isFinished} = this.props;
+        return(
+            <View>
+                <Text>
+                    Task name: {name}
+                </Text>
+                <Text>
+                    Task description: {description}
+                </Text>
+                <Text>
+                    is Task finished?: {isFinished.toString()}
+                </Text>
+            </View>
+        )
+    }
+}
+
 
 export default Viewable_Boards; 
