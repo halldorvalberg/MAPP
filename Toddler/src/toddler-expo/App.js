810dd66c88182d85fe2429bed124a0517d333b02
Halldór Valberg <halldorvs17@ru.is>
2019-11-26 13:04:27 +0000

    Read data and view through component

--- Toddler/src/toddler-expo/App.js
+++ Toddler/src/toddler-expo/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
+import Viewable_Boards from './components/board';
 
-export default class HelloWorldApp extends Component {
-    render() {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello, world!</Text>
-      </View>
-    );
-  }
+const data = require('./data/data.json');
+
+export default class App extends React.Component {
+    state = {
+        boards: [
+            { id: data.boards[0].id, name: data.boards[0].name, thumbnailPhoto: data.boards[0].thumbnailPhoto},
+            { id: data.boards[1].id, name: data.boards[1].name, thumbnailPhoto: data.boards[1].thumbnailPhoto},
+            { id: data.boards[2].id, name: data.boards[2].name, thumbnailPhoto: data.boards[2].thumbnailPhoto}
+        ]
+    }
+
+    render () {
+        return(
+            <View style={styles.container}> 
+            <Text> Something </Text>
+            </View>
+        );
+    }
 }
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        backgroundColor: 'lightskyblue',
+        alignItems: 'center',
+        justifyContent: 'center'
+
+    }
+});
---
+++ Toddler/src/toddler-expo/components/board/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class Viewable_Boards extends React.Component {
+    reder() {
+        return(
+            <View>
+                <Text>
+                    Boards goes here
+                </Text>
+            </View>
+        )
+    }
+}
+
+export default Viewable_Boards; 
