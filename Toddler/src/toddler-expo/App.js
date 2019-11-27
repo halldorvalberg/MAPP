bc41b4913ca1b3a52b57522120882614f76472d4
Halldór Valberg <halldorvs17@ru.is>
2019-11-27 11:32:41 +0000

    Merge conflict solved

--- Toddler/src/toddler-expo/App.js
+++ Toddler/src/toddler-expo/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-<<<<<<< HEAD
-import { View, StyleSheet,SafeAreaView, ScrollView } from 'react-native';
+import { SafeAreaView, ScrollView } from 'react-native';
 import Viewable_Boards from './components/board';
-import data from './data/data.json';
 import styles from './styles/style.js';
-=======
-import { ScrollView, SafeAreaView, StyleSheet } from 'react-native';
-import Viewable_Boards from './components/board';
->>>>>>> 2fcf7198803719e68cb295dbb694ec34f21aa46d
 
 export default class App extends React.Component {
     render() {
         return(
             <SafeAreaView style={styles.container}>
-<<<<<<< HEAD
                 <ScrollView >
-=======
-                <ScrollView style={styles.scrollView}>
->>>>>>> 2fcf7198803719e68cb295dbb694ec34f21aa46d
                     <Viewable_Boards/>
                 </ScrollView>
             </SafeAreaView>
@@ -25,20 +15,3 @@ export default class App extends React.Component {
     }
 }
 
-<<<<<<< HEAD
-=======
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		flexDirection: 'row',
-		flexWrap: 'wrap',
-		backgroundColor: 'lightskyblue',
-		alignItems: 'center',
-		justifyContent: 'center'
-    },
-    scrollView: {
-      backgroundColor: 'pink',
-      marginHorizontal: 20,
-    }
-});
->>>>>>> 2fcf7198803719e68cb295dbb694ec34f21aa46d
