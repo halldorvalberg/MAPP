08e8f3c1af1dbf17c8146bd966b3bb0bfd4e5097
Halldór Valberg <halldorvs17@ru.is>
2019-11-27 10:51:56 +0000

    Content Scrollable and
    Scrolling added so that all contend on display can be viewed

--- Toddler/src/toddler-expo/App.js
+++ Toddler/src/toddler-expo/App.js
@@ -1,37 +1,30 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { ScrollView, SafeAreaView, StyleSheet } from 'react-native';
 import Viewable_Boards from './components/board';
-import data from './data/data.json'
 
 export default class App extends React.Component {
-    state = {
-        boards: [
-            { id: data.boards[0].id, name: data.boards[0].name, thumbnailPhoto: data.boards[0].thumbnailPhoto},
-            { id: data.boards[1].id, name: data.boards[1].name, thumbnailPhoto: data.boards[1].thumbnailPhoto},
-            { id: data.boards[2].id, name: data.boards[2].name, thumbnailPhoto: data.boards[2].thumbnailPhoto}
-        ]
-    }
-
     render() {
-        const {boards} = this.state;
         return(
-            <View style={styles.container}>
-                <Viewable_Boards/>
-            </View>
+            <SafeAreaView style={styles.container}>
+                <ScrollView style={styles.scrollView}>
+                    <Viewable_Boards/>
+                </ScrollView>
+            </SafeAreaView>
         );
     }
 }
 
-
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1, 
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        backgroundColor: 'lightskyblue',
-        alignItems: 'center',
-        justifyContent: 'center'
-
+	container: {
+		flex: 1,
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+		backgroundColor: 'lightskyblue',
+		alignItems: 'center',
+		justifyContent: 'center'
+    },
+    scrollView: {
+      backgroundColor: 'pink',
+      marginHorizontal: 20,
     }
 });
