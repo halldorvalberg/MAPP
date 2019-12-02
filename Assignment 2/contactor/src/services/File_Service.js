0457d6c8feb172febbeff634b9a19f57289cd869
ingi17 <ingi17@ru.is>
2019-12-02 12:59:46 +0000

    Services init

---
+++ Assignment 2/contactor/src/services/File_Service.js
@@ -0,0 +1,4 @@
+import * as FileSystem from 'expo-file-system';
+
+const dir = FileSystem.documentDirectory
+
---
+++ Assignment 2/contactor/src/services/Image_Service.js
@@ -0,0 +1,3 @@
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
