91ecf644c58444abdfb732575ab4001d5ede5cb8
Halldór Valberg <halldorvs17@ru.is>
2019-12-03 12:10:58 +0000

    Buttons added to detail screen
    Functionality missing. Needs implementation

--- Assignment 2/contactor/src/views/Contact_Detail/index.js
+++ Assignment 2/contactor/src/views/Contact_Detail/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, Button, Alert} from 'react-native'
 import styles from "../../style.js"
 
 class Contact_Detail extends React.Component {
@@ -9,6 +9,7 @@ class Contact_Detail extends React.Component {
     render() {
         const { contact } = this.props.navigation.state.params;
         return (
+            // Spurning að henda eftirfarandi virkni í component. Gæti verið meira nice
             <View style={styles.container}>
                 {
                     contact.imageAvailable
@@ -28,6 +29,20 @@ class Contact_Detail extends React.Component {
                     <Text>
                         {contact.phoneNumbers[0].number}
                     </Text>
+
+                    <View>
+                    <Button
+                        title={'Call'}
+                        onPress={() => Alert.alert("This button should open the phone app and allow you to call the user... but it does't ... yet")}
+                    />
+                </View>
+                </View>
+                
+                <View>
+                    <Button
+                        title={'Edit User'}
+                        onPress={() => Alert.alert('Here we will put the edit functionality')}
+                    />
                 </View>
             </View>
         )
