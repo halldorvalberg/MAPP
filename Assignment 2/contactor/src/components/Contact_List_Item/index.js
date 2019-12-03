0839ee05b0f1d7152a7bbf196124c4ae436284cb
Hlynur Holm Hauksson <hlynurholm97@gmail.com>
2019-12-03 10:37:17 +0000

    commit to pull

--- Assignment 2/contactor/src/components/Contact_List_Item/index.js
+++ Assignment 2/contactor/src/components/Contact_List_Item/index.js
@@ -3,20 +3,21 @@ import { connect } from 'react-redux'
 import { withNavigation } from 'react-navigation'
 
 import { Text, View, Image } from 'react-native'
-import { TouchableHighlight } from 'react-native-gesture-handler'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import styles from '../../style.js'
 
 const Contact_List_Item = ({ contact, navigation: {navigate} }) => {
     return (
-        <TouchableHighlight
+        <TouchableOpacity
             onPress={() => navigate('Contact_Detail', {name: contact.name, phone_number: contact.phone_number, image: contact.image}) }
         >
-            <View>
-                <Text> {contact.name} </Text>
+            <View style={styles.flat_list_item}>
                 <Image source={{ uri: contact.image }}
                     resizeMode="cover"
-                    style={{ height: 100, width: 100 }} />
+                    style={styles.flat_list_item_image} />
+                <Text style={styles.flat_list_item_text}> {contact.name} </Text>
             </View>
-        </TouchableHighlight>
+        </TouchableOpacity>
     )
 }
 
