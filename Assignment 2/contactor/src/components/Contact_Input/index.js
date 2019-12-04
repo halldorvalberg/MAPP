255036dd455f59b80767bc991dcc7afa4634f789
Halldór Valberg <halldorvs17@ru.is>
2019-12-04 10:56:19 +0000

    Bugfix: No image in edit crash

--- Assignment 2/contactor/src/components/Contact_Input/index.js
+++ Assignment 2/contactor/src/components/Contact_Input/index.js
@@ -14,17 +14,28 @@ export default class Contact_Input extends React.Component {
 
     componentWillMount(){
         const obj = this.props.contact_obj
-        this.setState({name: obj.name, number: obj.phoneNumbers[0].number, image: obj.image.uri })
+        console.log(obj.contact.phoneNumbers[0].number)
+        if(obj.contact.imageAvailable){
+            this.setState({name: obj.contact.name, number: obj.contact.phoneNumbers[0].number, image: obj.contact.image.uri })
+        }
+        else {
+            this.setState({name: obj.contact.name, number: obj.contact.phoneNumbers[0].number, image: '' })
+        }
     }
 
     _submit_pressed() {
         const obj = this.props.contact_obj
-        obj.name = this.state.name
-        obj.phoneNumbers[0].number = this.state.number
-        obj.image.uri = this.state.image
+        obj.contact.name = this.state.name
+        obj.contact.phoneNumbers[0].number = this.state.number
+        obj.contact.image.uri = this.state.image
 
         this.props._submit_function(obj)
     }
+
+    /**
+     * TODO 
+     * Crashes if object has null as image
+     */
 
     render() {
         const {name, number, image} = this.state
