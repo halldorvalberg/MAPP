8f32bfbce7f10cd22872dce47b0f3c4a721c6604
ingi17 <ingi17@ru.is>
2019-12-12 13:00:40 +0000

    something fixed dno what

--- Assignment 3/dr.cinema/src/Services/movie_service.js
+++ Assignment 3/dr.cinema/src/Services/movie_service.js
@@ -2,16 +2,22 @@ export const get_cinema = async (cinemas, id) => {
     return cinemas.filter(c => c.id === id);
 }
 
-export const get_movies_by_cinema = async (movies, id) => {
-    console.log(id)
-    console.log(movies[0])
+export const get_movies_by_cinema = (movies, id) => {
+
     // console.log(movies.filter(x => x.showtimes.cinema.id === id ));
-    const movie = movies.filter(m => {
+
+    var arr = [];
+    movies.filter(m => {
+        // console.log(m);
         return m.showtimes.forEach(c => {
-            return c.id === id;
+            if (c.cinema.id == id) {
+                arr.push(c);
+            }
+            return c.cinema.id == id;
         })
     });
-    console.log(movie);
+    console.log("MOVIE ------ ARRAY");
+    console.log(arr);
     return movie;
 }
 
